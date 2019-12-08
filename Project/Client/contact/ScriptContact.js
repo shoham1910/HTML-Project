10126271fad644f5005b212245e0f1a4c7f393d2
Tamir Alalouf <tamir1020@gmail.com>
2019-12-08 14:50:37 +0200

    the fields are checked now

--- Project/Client/contact/ScriptContact.js
+++ Project/Client/contact/ScriptContact.js
@@ -1,53 +1,57 @@
 function sendOK()
 {
+	var str="Please Enter Correct:";
 	var name=document.getElementById("name").value;
 	var phone=document.getElementById("phone").value;
 	var mail=document.getElementById("Email").value;
-	var com=document.getElementById("comment").value;
+	var flag=false;
+	var cnt=0;
 	var matches = name.match(/\d+/g);
-	if (name.length==0 || matches != null) {
-    	document.getElementById("name").style.borderColor="#712b29";
+	if (name.length<1 || matches != null) {
+		document.getElementById("name").style.borderColor="#712b29";
+		str+=" Name";
+		flag=true;
+		cnt++;
 	}
 	else{
-		document.getElementById("name").style.borderColor=
+		document.getElementById("name").style.borderColor="initial";
 	}
-	if(pass.length<6)
+	if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))){
+		document.getElementById("Email").style.borderColor="#712b29";
+		if(flag) str+=", Email";
+		else str+=" Email";
+		flag=true;
+		cnt++;
+	}
+	if (!(/^\d{10}$/)){
+		document.getElementById("phone").style.borderColor="#712b29";
+		if(flag) str+=", Phone";
+		else str+=" Phone";
+		flag=true;
+		cnt++;
+	}
+	else{
+		document.getElementById("phone").style.borderColor="initial";
+	}
+
+	
+	if(flag){ 
+		document.getElementById("alert-msg").textContent = str+".";
 		return false;
-	var i,up=0,low=0,num=0,spacial=0,l;
-	var n=/[0-9]/;
-	var upper=/[A-Z]/;
-	var lower=/[a-z]/;
-	var letter=/[a-zA-Z]/
-	for(i=0;i<pass.length;i++)
-	{
-		l=pass.charAt(i);
-		if(pass.charAt(i).match(n))
-		{
-			if(num==0)
-				num=1;
-		}
-		else if(pass.charAt(i).match(letter))
-		{		
-			if(up==0 && pass.charAt(i)==pass.charAt(i).toUpperCase())
-				up=1;
-			if(low==0 && pass.charAt(i)==pass.charAt(i).toLowerCase())
-				low=1;
-		}	
-		else if(spacial==0) spacial=1;
-		if(num==1 && low==1 && up==1 &&  spacial==1) return true;
 	}
-	return false;
+	return true;
 }
+
 function showAlert()
 {
-	var name=document.getElementById("username").value;
-	var pass=document.getElementById("password").value;
-	if(name.length<3 || name.split().length!=1 || !passwordOK()){
+	var name=document.getElementById("name").value;
+	var phone=document.getElementById("phone").value;
+	var mail=document.getElementById("Email").value;
+	if(!sendOK()){
 		document.getElementById("alert-msg").style.visibility = "visible";
 		return false;
 	}
 	document.getElementById("alert-msg").style.visibility = "hidden";
-	window.alert("Username: "+name+"\n"+"Password: "+pass);
 	return true;
 }
 
